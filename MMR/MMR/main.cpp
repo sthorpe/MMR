68547b34db8133916a2f9ddb0e812c5b42381d28
Scott Thorpe <sthorpe@gmail.com>
2019-04-18 13:43:16 -0400

    First crack at outline of code.

---
+++ MMR/MMR/main.cpp
@@ -0,0 +1,131 @@
+//
+//  main.cpp
+//  MMR
+//
+//  Created by Scott Thorpe on 4/18/19.
+//  Copyright © 2019 Scott Thorpe. All rights reserved.
+//
+
+#include <iostream>
+#include <boost/regex.hpp>
+
+using namespace std;
+
+void get(int leafIndex) {
+    cout << "Mutating these values: " << endl << "leafLength, leafPosition, localPeakPosition, localPeakValue, leafValue" << endl << endl;
+}
+
+void _getNode(int position) {
+    cout << "Here's the position: " << position << endl << endl;
+}
+
+void _put(int value, int nodeIndex, int localPeakPosition) {
+    cout << "This function takes three values" << endl << endl;
+}
+
+void getRoot(int leafIndex) {
+    cout << "This returns the leafIndex: " << leafIndex << endl << endl;
+}
+
+void _verifyPath() {
+    
+}
+
+void getNodeLength() {
+    
+}
+
+void getLeafLength() {
+    
+}
+
+void del() {
+    
+}
+
+void _setLeafLength() {
+    
+}
+
+void _hashUp() {
+    
+}
+
+static void getNodeLength(int leafLength) {
+    
+}
+
+void leftChildPosition() {
+    
+}
+
+void rightChildPosition() {
+    
+}
+
+void siblingPosition() {
+    
+}
+
+void parentIndex() {
+    
+}
+
+static void peakPositions() {
+    
+}
+
+static void localPeakPosition() {
+    
+}
+
+static void _localPeakPosition() {
+    
+}
+
+static void mountainPositions() {
+    
+}
+
+static void superUmbrellaPeakFromLeafIndex() {
+    
+}
+
+static void getNodePosition() {
+    
+}
+
+void getProof(){
+    
+}
+
+static void getProofPositions() {
+    
+}
+
+void _removeImpledNodes() {
+    
+}
+
+void _hasNode() {
+    
+}
+
+void print() {
+    
+}
+
+int main(int argc, const char * argv[]) {
+    int leafIndex = 0;
+    int position = 2;
+    
+    int value = 0;
+    int nodeIndex = 0;
+    int localPeakPosition = 0;
+    
+    get(leafIndex);
+    _put(value, nodeIndex, localPeakPosition);
+    _getNode(position);
+    getRoot(leafIndex);
+    return 0;
+}
